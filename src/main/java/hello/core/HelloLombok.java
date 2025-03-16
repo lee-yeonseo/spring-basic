04652f1a056a2d8a4d7c86faccf46a2edd5a8638
lee-yeonseo <dlth010@naver.com>
2025-03-16 22:58:05 +0900

    lombok

---
+++ src/main/java/hello/core/HelloLombok.java
@@ -0,0 +1,22 @@
+package hello.core;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.stereotype.Service;
+
+@Getter
+@Setter
+@ToString
+public class HelloLombok {
+
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+        HelloLombok helloLombok = new HelloLombok();
+        helloLombok.setName("asdfas");
+
+        System.out.println("helloLombok = " + helloLombok);
+    }
+}
